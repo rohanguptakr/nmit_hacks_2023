12f6bb074808d324c435a149452b2ac1739752d3
rohanguptakr <109743454+rohanguptakr@users.noreply.github.com>
2022-11-02 11:10:17 +0530

    initial commit

---
+++ components/Home/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+function landingPage(){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default landingPage;
---
+++ components/tracks/tracks.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+function tracks(){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default tracks
